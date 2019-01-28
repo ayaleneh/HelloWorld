b8928a24c43f090601432a9b37b04586295a0120
ayaleneh <getaunayaleneh@gmail.com>
2019-01-28 16:28:40 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("HELLOWORLD");
+        int num=0;
+        Scanner scanner=new Scanner(System.in);
+        //scanner.nextInt();
+        System.out.println("enter a number");
+        num=scanner.nextInt();
+        System.out.println("the number is "+num);
+    }
+}
